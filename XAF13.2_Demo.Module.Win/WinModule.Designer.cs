6d15dec1e762c432af2994dfe73a8e93cddc153d
m.grundner <m.grundner@delegate.at>
2013-12-06 15:18:41 +0100

    init checkin

---
+++ XAF13.2_Demo.Module.Win/WinModule.Designer.cs
@@ -0,0 +1,41 @@
+namespace XAF13._2_Demo.Module.Win
+{
+    partial class _2_DemoWindowsFormsModule
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // 
+            // _2_DemoWindowsFormsModule
+            // 
+            this.RequiredModuleTypes.Add(typeof(XAF13._2_Demo.Module._2_DemoModule));
+            this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule));
+
+        }
+
+        #endregion
+    }
+}
